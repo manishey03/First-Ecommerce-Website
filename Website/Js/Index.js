e419ecfd8356c2a89a1d0aa3a9f3a28559229dd2
MANISHA KC <manishakc@MANISHAs-MacBook-Air.local>
2023-07-26 10:51:28 +0545

    web dev

---
+++ Website/Js/Index.js
@@ -0,0 +1,7 @@
+const scrollTopButton = document.querySelector('.scroll-top'); //button selected
+scrollTopButton.addEventListener('click', ()=>{
+    window.scrollTo(0, 0) //x=0 y=0
+})  //adding event listener and the event is click to reun function
+
+    
+    
